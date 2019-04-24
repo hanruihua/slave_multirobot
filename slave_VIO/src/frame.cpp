3245d17ef87cc7ff821c191f45f4f1c6595410af
Herry <744475118@qq.com>
2019-04-24 17:09:20 +0800

    slave VIO

--- slave_VIO/src/frame.cpp
+++ slave_VIO/src/frame.cpp
@@ -1,1 +1,14 @@
 #include "slaveslam/frame.h"
+namespace myslam
+{
+    Frame::Frame()
+    : id_(-1), time_stamp_(-1), camera_(nullptr)
+    {
+
+    }
+
+    Frame::Frame ( long id, double time_stamp, SE3 T_c_w, Camera::Ptr camera, Mat color, Mat depth )
+    : id_(id), time_stamp_(time_stamp), T_c_w_(T_c_w), camera_(camera), color_(color), depth_(depth)
+    {
+
+    }   
