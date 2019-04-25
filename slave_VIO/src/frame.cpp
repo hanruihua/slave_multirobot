80da0c36d46d25cd7bb04aaa57584363cca91c4e
Herry <744475118@qq.com>
2019-04-25 16:32:40 +0800

    slave VIO

--- slave_VIO/src/frame.cpp
+++ slave_VIO/src/frame.cpp
@@ -22,4 +22,48 @@ namespace myslam
     {
         static long factory_id = 0;
         return Frame::Ptr( new Frame(factory_id++) );
-    }     
+    }
+
+    double Frame::findDepth(const cv::KetPoint& kp)
+    {
+        int x = cvRound(kp.pt.x);
+        int y = cvRound(kp.pt.y);
+        ushort d = depth_.ptr<ushort>(y)[x];
+        if ( d!=0 )
+        {
+            return double(d)/camera_->depth_scale_;
+        }
+        else 
+        {
+            // check the nearby points 
+            int dx[4] = {-1,0,1,0};
+            int dy[4] = {0,-1,0,1}; 
+            for ( int i=0; i<4; i++ )
+            {
+                d = depth_.ptr<ushort>( y+dy[i] )[x+dx[i]];
+                if ( d!=0 )
+                {
+                    return double(d)/camera_->depth_scale_;
+                }
+            }
+        }
+        return -1.0;
+    }
+
+    Vector3d Frame::getCamCenter() const
+    {
+        return T_c_w_.inverse().translation();
+    }
+
+    bool Frame::isInFrame ( const Vector3d& pt_world )
+    {
+        Vector3d p_cam = camera_->world2camera( pt_world, T_c_w_ );
+        if ( p_cam(2,0)<0 ) 
+            return false;
+        Vector2d pixel = camera_->world2pixel( pt_world, T_c_w_ );
+        return pixel(0,0)>0 && pixel(1,0)>0 
+            && pixel(0,0)<color_.cols 
+            && pixel(1,0)<color_.rows;
+    }
+
+}
