ec58b591267c8c1913f26bc9e5f60acd9cc30dc4
Herry <744475118@qq.com>
2019-09-18 16:15:25 +0800

    update

--- slave_msgs/src/msg_trans_node.cpp
+++ slave_msgs/src/msg_trans_node.cpp
@@ -8,7 +8,8 @@
 
 
 
-std::string robot_id;
+std::string robot_id, id;
+
 geometry_msgs::Vector3  robot_linear;
 geometry_msgs::Vector3  robot_angular;
 geometry_msgs::Twist msg;
@@ -20,7 +21,7 @@ void robotControl_callback( slave_msgs::slave_VelocityID msgInput)
    robot_linear = msgInput.linear;
    robot_angular = msgInput.angular;
    ROS_INFO("The receive control is %s,%f,%f",robot_id.data(), robot_linear.x, robot_angular.z);
-   if(robot_id=="1"){//check msg receiver
+   if(robot_id==id){//check msg receiver
       
       msg.linear = robot_linear;
       msg.angular = robot_angular;
@@ -35,8 +36,16 @@ int main(int argc, char **argv)
 {
   
   ros::init(argc, argv, "msg_trans_node");
-  ros::NodeHandle n;
-
+  ros::NodeHandle n("~");
+  std::string id="default";
+		
+	if (!n.hasParam("id"))
+	{
+		ros::param::param<std::string>("id", id, "0");	
+		ROS_INFO("No id set, default 0");
+	}	
+  n.getParam("id",id);
+  std::cout << "id are set to " << id  << std::endl;
   ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1000);
   ros::Subscriber sub1=n.subscribe("/multiKey/cmd_vel", 1000, robotControl_callback);
 	
@@ -48,7 +57,7 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
     chatter_pub.publish(msg);
-    ROS_INFO("Published");
+    //ROS_INFO("Published");
     ros::spinOnce();
  
     loop_rate.sleep();
