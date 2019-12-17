caf8a02a1ffa42455f35d0950fc35567e0ded7ee
Chenshengduo <1134600061@qq.com>
2019-12-17 19:54:10 +0800

    delete old driver

--- driver/nlink/src/NProtocol/nprotocolitembase.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "nprotocolitembase.h"
-
-bool NProtocolItemBase::verifyData(const char *) { return true; }
-
-bool NProtocolItemBase::updateItemBytes(const char *) { return true; }
--- driver/nlink/src/serial/initserial.h
+++
@@ -1,7 +0,0 @@
-﻿#ifndef INITSERIAL_H
-#define INITSERIAL_H
-#include "serial/serial.h"
-
-void initSerial(int argc, char **argv, serial::Serial *serial);
-
-#endif // INITSERIAL_H
