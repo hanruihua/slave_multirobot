7f1e3356ed9a9dfcc0a7ed6352c2c1d95a42137d
han <744475118@qq.com>
2020-05-29 11:18:54 +0800

    slave msgs

--- slave_msgs/setup.py
+++ slave_msgs/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['msgs_lib'],
-	package_dir={'':''}
+    packages=['ekf_localization'],
+    package_dir={'':'msgs_lib'}
 )
 
 setup(**setup_args)
