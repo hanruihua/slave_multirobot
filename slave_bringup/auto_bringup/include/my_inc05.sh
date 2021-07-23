6001789418042ed805e8d85d8f0016fca42f4879
hanruihua <744475118@qq.com>
2021-07-23 21:27:14 +0800

    update ip

--- slave_bringup/auto_bringup/include/my_inc05.sh
+++ slave_bringup/auto_bringup/include/my_inc05.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 export ROS_MASTER_URI=http://192.168.1.37:11311
-export ROS_HOSTNAME=192.168.1.42
-export ROS_IP=192.168.1.42
+export ROS_HOSTNAME=192.168.1.27
+export ROS_IP=192.168.1.27
 export NAME_SPACE=agent5
