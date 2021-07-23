cb491013aaebf668640e08fdd3668aedf38f499f
airine <airine0119@gmail.com>
2021-07-23 21:05:19 +0800

    update ip

--- slave_bringup/auto_bringup/include/my_inc04.sh
+++ slave_bringup/auto_bringup/include/my_inc04.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-export ROS_MASTER_URI=http://192.168.0.123:11311
-export ROS_HOSTNAME=192.168.0.116
-export ROS_IP=192.168.0.116
+export ROS_MASTER_URI=http://192.168.1.37:11311
+export ROS_HOSTNAME=192.168.1.80
+export ROS_IP=192.168.1.80
 export NAME_SPACE=agent4
 
